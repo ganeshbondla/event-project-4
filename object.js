df2f2c94797c05c6411c8944072455d8fe111c82
Bondla Ganesh <60213313+ganeshbondla@users.noreply.github.com>
2024-05-01 19:22:02 +0530

    Added condition

--- object.js
+++ object.js
@@ -5,11 +5,20 @@ let user = {
   address: "123 Main St",
   city: "Anytown",
   state: "CA",
+  ele: true,
   fullName: function () {
     return this.firstName + " " + this.lastName;
   },
 };
 
 user.zipcode = "12345";
+
+let finalResult;
+
+if (user.age >= 30) {
+  finalResult = user.fullName();
+} else {
+  console.log("Age is less than 30");
+}
 
 console.log(user);
